11214319034f95d0d4b4c2405d831876d8709490
Carlos <crrlos1801@gmail.com>
2015-11-03 08:06:55 -0600

    modificado el controlador para que se comporte como una clase

--- escritorio/controladores/controladorPago.php
+++ escritorio/controladores/controladorPago.php
@@ -1,65 +1,68 @@
 <?php
 
 require_once 'config.php';
-/* panel de control del controlador de pagos */
-if (isset($_POST['payment_status'])) {//verifica si fue invocado por Paypal
-   
-    if($_POST['payment_status'] == 'Completed')
-        procesarPagoCompletado();
-    
-} else {
-    //fue invocado por la aplicación
-    $opcion = isset($_POST['oppago']) ? $_POST['oppago'] : '';
-    switch ($opcion) {
-        case 1:
-            procesarFormaDePago();
-            break;
-        default :
-            mostrarDetalles();
+
+class ControladorPago {
+
+    function procesarPagoCompletado() {
+
+        $idusuario = $_POST['custom'];
+        $pedido = PedidosQuery::create()->filterByEstado('pendiente')->filterByIdcliente($idusuario)->findOne();
+        $pedido->setEstado('cancelado');
+        $pedido->save();
+        $venta = new Ventas();
+        $venta->setIdpedido($pedido->getIdpedido());
+        $venta->setFecha(date('Y-m-d H:i:s'));
+        $venta->setFormapago('PAYPAL');
+        $venta->setCodigotransaccion($_POST['txn_id']);
+        $venta->save();
     }
-}
 
+    function procesarFormaDePago() {
+        /* pago por paypal */
+        if ($_POST['formadepago'] == 1) {/* pago por paypal */
 
-/* funciones para controlar las operaciones*/
-function mostrarDetalles() {
-    include __DIR__ . '/../paginas/detallePago.php';
-}
+            if (isset($_SESSION['usuario'])) {
+                $idusuario = UsuariosQuery::create()->findOneByNombreusuario($_SESSION['usuario'])->getIdusuario();
+                $pedido = PedidosQuery::create()->filterByIdcliente($idusuario)->filterByEstado('pendiente')->findOne();
+                $detallePedido = $pedido != null ? $pedido->getDetallepedidoss() : null;
+                if ($detallePedido != null) {
+                    include __DIR__ . '/../paginas/generarPaypal.php';
+                }
+            }
+        } elseif ($_POST['formadepago'] == 2) {/* pago por depósito o transferencia bancaria */
+        }
+    }
 
+    function mostrarDetalles() {
+        include __DIR__ . '/../paginas/detallePago.php';
+    }
 
-//se encarga de cargar la página adecuada al tipo de pago que se realizará
-function procesarFormaDePago() {
-    /* pago por paypal */
-    if ($_POST['formadepago'] == 1) {/*pago por paypal*/
-
-        if (isset($_SESSION['usuario'])) {
-            $idusuario = UsuariosQuery::create()->findOneByNombreusuario($_SESSION['usuario'])->getIdusuario();
-            $pedido = PedidosQuery::create()->filterByIdcliente($idusuario)->filterByEstado('pendiente')->findOne();
-            $detallePedido = $pedido != null ? $pedido->getDetallepedidoss() : null;
-            if ($detallePedido != null) {
-                include __DIR__ . '/../paginas/generarPaypal.php';
+    function iniciar() {
+        if (isset($_POST['payment_status'])) {//verifica si fue invocado por Paypal
+            if ($_POST['payment_status'] == 'Completed')
+                procesarPagoCompletado();
+        } else {
+            //fue invocado por la aplicación
+            $opcion = isset($_POST['oppago']) ? $_POST['oppago'] : '';
+            switch ($opcion) {
+                case 1:
+                    $this->procesarFormaDePago();
+                    break;
+                default :
+                    $this->mostrarDetalles();
             }
         }
-    } elseif ($_POST['formadepago'] == 2) {/*pago por depósito o transferencia bancaria*/
+    }
+    
+    function __destruct() {
         
     }
+
 }
 
-/* funciones paypal*/
-function procesarPagoCompletado(){
-    
-   $idusuario = $_POST['custom'];
-   
+$controlador = new ControladorPago();
+$controlador->iniciar();
 
-   $pedido = PedidosQuery::create()->filterByEstado('pendiente')->filterByIdcliente($idusuario)->findOne();
-   $pedido->setEstado('cancelado');
-   $pedido->save();
-   
-   $venta = new Ventas();
-   $venta->setIdpedido($pedido->getIdpedido());
-   $venta->setFecha(date('Y-m-d H:i:s'));
-   $venta->setFormapago('PAYPAL');
-   $venta->setCodigotransaccion($_POST['txn_id']);
-   $venta->save();
-    
-}
-/*-------------------*/
+
+
